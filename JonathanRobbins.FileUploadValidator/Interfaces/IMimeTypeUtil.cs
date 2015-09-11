8671d9a7f250e1e1b06f42c9d496801b1fc7641f
islaytitans <jonathan.m.robbins@gmail.com>
2015-09-11 16:28:59 +0100

    Dev / Completed the change to use MimeTypes defined as Sitecore items for the checking of the uplaoded file mime type

---
+++ JonathanRobbins.FileUploadValidator/Interfaces/IMimeTypeUtil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JonathanRobbins.FileUploadValidator.IO;
+using JonathanRobbins.FileUploadValidator.Models;
+
+namespace JonathanRobbins.FileUploadValidator.Interfaces
+{
+    public interface IMimeTypeUtil
+    {
+        /// <summary>
+        /// Gets the MimeType of a file from a list of constant MimeType
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        string GetMimeType(byte[] file, string fileName);
+        /// <summary>
+        /// Determines if the file's MimeType is contained within the permitted MimeTypes
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="fileName"></param>
+        /// <param name="permittedMimeTypes"></param>
+        /// <returns>FileType of the file if matched</returns>
+        FileType MimeTypeAllowed(byte[] file, string fileName, List<FileType> permittedMimeTypes);
+        /// <summary>
+        /// Creates a ByteArray from values in a Csv
+        /// </summary>
+        /// <param name="byteArrayString"></param>
+        /// <returns>ByteArray of the values</returns>
+        byte[] CsvToByteArray(string byteArrayString);
+    }
+}
--- JonathanRobbins.FileUploadValidator/Webforms/Validators/FileUploadValidator.cs
+++ JonathanRobbins.FileUploadValidator/Webforms/Validators/FileUploadValidator.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
 using JonathanRobbins.FileUploadValidator.Enums.IO;
+using JonathanRobbins.FileUploadValidator.Interfaces;
 using JonathanRobbins.FileUploadValidator.IO;
+using JonathanRobbins.FileUploadValidator.Models;
+using JonathanRobbins.FileUploadValidator.Webforms.Controls;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Form.Core.Validators;
@@ -17,6 +23,27 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Validators
 {
     public class FileUploadValidator : FormCustomValidator
     {
+        private IMimeTypeUtil _mimeTypeUtil = new MimeTypeUtil();
+
+        private Item _fieldItem;
+        private Item FieldItem
+        {
+            get
+            {
+                if (_fieldItem == null)
+                {
+                    string fieldId = this.classAttributes["fieldid"];
+
+                    if (!string.IsNullOrEmpty(fieldId))
+                    {
+                        _fieldItem = Sitecore.Context.Database.GetItem(fieldId);
+                    }
+                }
+
+                return _fieldItem;
+            }
+        }
+
         protected override bool EvaluateIsValid()
         {
             bool isValid = false;
@@ -43,8 +70,6 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Validators
             //bool validSize = ValidateFileSize(fileUploaded);
             //if (!validSize) return false;
 
-            //Branch commit test
-
             return true;
         }
 
@@ -54,12 +79,11 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Validators
 
             try
             {
-                Stream stream = postedFile.InputStream;
+                var permittedMimeTypes = DeterminePermitedMimeTypes();
 
-                var mimeTypeUtil = new MimeTypeUtil();
-                string mime = mimeTypeUtil.GetMimeType(ReadFully(stream), postedFile.FileName);
+                FileType uploadedFileType = _mimeTypeUtil.MimeTypeAllowed(ReadFully(postedFile.InputStream), postedFile.FileName, permittedMimeTypes);
 
-                valid = PermittedMimeTypes.Contains(mime);
+                valid = uploadedFileType != null;
             }
             catch (Exception ex)
             {
@@ -69,6 +93,42 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Validators
             }
 
             return valid;
+        }
+
+        private List<FileType> DeterminePermitedMimeTypes()
+        {
+            var fileTypes = new List<FileType>();
+            
+            if (FieldItem != null)
+            {
+                var itemIds = new List<string>();
+
+                var regexSelectedValue = new Regex(@"<selectedvalue>(.*?)</selectedvalue>".ToLower());
+                var selectedValueNodes = regexSelectedValue.Matches(FieldItem["Parameters"].ToLower());
+
+                if (selectedValueNodes.Count > 0)
+                {
+                    foreach (Match selectedValueNode in selectedValueNodes)
+                    {
+                        var regexItems = new Regex(@"<item>(.*?)</item>".ToLower());
+                        var itemNodes = regexItems.Matches(selectedValueNode.ToString());
+
+                        itemIds.AddRange(from Match itemNode in itemNodes
+                            where
+                                itemNode.Groups[1] != null && !string.IsNullOrEmpty(itemNode.Groups[1].Value) &&
+                                TryParseGuid(itemNode.Groups[1].Value)
+                            select itemNode.Groups[1].Value);
+                    }
+
+                    if (itemIds.Any())
+                    {
+                        var fileTypeItems = itemIds.Select(i => Sitecore.Context.Database.GetItem(i));
+                        fileTypes = fileTypeItems.Select(i => new FileType(i)).ToList();
+                    }
+                }
+            }
+
+            return fileTypes;
         }
 
         private static byte[] ReadFully(Stream input)
@@ -86,25 +146,6 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Validators
         }
 
         private IEnumerable<string> _permittedMimeTypes;
-
-        private IEnumerable<string> PermittedMimeTypes
-        {
-            get
-            {
-                if (_permittedMimeTypes == null)
-                {
-                    _permittedMimeTypes = new List<string>()
-                    {
-                        MimeType.ImageBmp,
-                        MimeType.ImageGif,
-                        MimeType.ImageJpeg,
-                        MimeType.ImagePng,
-                    };
-                }
-
-                return _permittedMimeTypes;
-            }
-        }
 
         private const int DefaultFileSizeLimit = 3000000;
 
@@ -142,5 +183,21 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Validators
 
         //    return (sizeInBytes <= FileSizeLimitInBytes);
         //}
+
+        public bool TryParseGuid(string guidString)
+        {
+            if (guidString == null) 
+                throw new ArgumentNullException("guidString");
+            
+            try
+            {
+                var guid = new Guid(guidString);
+                return true;
+            }
+            catch (FormatException)
+            {
+                throw new Exception("Failed to parse the Guid of File Type Item - ensure the Value field of the Items property of the SecureFileUpload WFFM Field is set to __Id");
+            }
+        }
     }
 }
