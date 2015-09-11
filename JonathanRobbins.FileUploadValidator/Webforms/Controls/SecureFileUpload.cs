5aeecd2b95b6420612a237e26153455303bb650d
islaytitans <jonathan.m.robbins@gmail.com>
2015-09-11 16:52:55 +0100

    Dev / Added max file size limit

--- JonathanRobbins.FileUploadValidator/Webforms/Controls/SecureFileUpload.cs
+++ JonathanRobbins.FileUploadValidator/Webforms/Controls/SecureFileUpload.cs
@@ -6,6 +6,7 @@ using Sitecore.Form.UI.Adapters;
 using Sitecore.Form.UI.Converters;
 using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.UI;
@@ -16,14 +17,24 @@ using ListItemCollection = System.Web.UI.WebControls.ListItemCollection;
 
 namespace JonathanRobbins.FileUploadValidator.Webforms.Controls
 {
-    [Adapter(typeof(FileUploadAdapter))]
-    public class SecureFileUpload : List
+    [Adapter(typeof(ListControlAdapter))]
+    [ListAdapter("Items", typeof(ListItemsAdapter))]
+    //[Adapter(typeof(FileUploadAdapter))]
+    public class SecureFileUpload : ValidateControl, IHasTitle
     {
         private static readonly string baseCssClassName = "scfFileUploadBorder";
         protected Panel generalPanel = new Panel();
         protected System.Web.UI.WebControls.Label title = new System.Web.UI.WebControls.Label();
         protected FileUpload upload = new FileUpload();
-        private string uploadDir = "/sitecore/media library";
+        private int _fileSizeLimit;
+        private ListItemCollection _items;
+        private ListItemCollection _selectedItems;
+        private string _uploadDir = "/sitecore/media library";
+
+        //protected System.Web.UI.WebControls.ListControl InnerListControl
+        //{
+        //    get { throw new NotImplementedException(); }
+        //}
 
         public override string ID
         {
@@ -42,8 +53,8 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Controls
         [VisualCategory("Upload")]
         public string UploadTo
         {
-            get { return this.uploadDir; }
-            set { this.uploadDir = value; }
+            get { return this._uploadDir; }
+            set { this._uploadDir = value; }
         }
 
         public override ControlResult Result
@@ -83,13 +94,13 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Controls
             get { return (Control)this.generalPanel; }
         }
 
-        public SecureFileUpload2(HtmlTextWriterTag tag)
+        public SecureFileUpload(HtmlTextWriterTag tag)
             : base()
         {
-            this.CssClass = SecureFileUpload2.baseCssClassName;
+            this.CssClass = SecureFileUpload.baseCssClassName;
         }
 
-        public SecureFileUpload2()
+        public SecureFileUpload()
             : this(HtmlTextWriterTag.Div)
         {
         }
@@ -115,6 +126,100 @@ namespace JonathanRobbins.FileUploadValidator.Webforms.Controls
             this.Controls.AddAt(0, (Control)this.title);
             this.generalPanel.Controls.AddAt(0, (Control)this.help);
             this.generalPanel.Controls.AddAt(0, (Control)this.upload);
+            this.InitItems(this._items);
+        }
+
+        [PersistenceMode(PersistenceMode.InnerProperty)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [Browsable(false)]
+        [VisualCategory("List")]
+        [VisualFieldType(typeof(ListField))]
+        [TypeConverter(typeof(ListItemCollectionConverter))]
+        [Description("Collection of items.")]
+        [VisualProperty("Items:", 100)]
+        public ListItemCollection Items
+        {
+            get
+            {
+                return this._items;
+            }
+            set
+            {
+                this._items = value;
+            }
+        }
+
+        [TypeConverter(typeof(ListItemCollectionConverter))]
+        [VisualFieldType(typeof(MultipleSelectedValueField))]
+        [VisualCategory("List")]
+        [Browsable(false)]
+        [VisualProperty("Selected Values:", 450)]
+        public ListItemCollection SelectedValue
+        {
+            get
+            {
+                return this._selectedItems;
+            }
+            set
+            {
+                this._selectedItems = value;
+            }
+        }
+
+        public override string DefaultValue
+        {
+            set
+            {
+                //this.SelectedValue = (ListItemCollection)value;
+            }
+        }
+
+        protected virtual void InitItems(ListItemCollection collection)
+        {
+            if (collection == null)
+                collection = new ListItemCollection();
+            //this.InnerListControl.Items.Clear();
+            //this.InnerListControl.Items.AddRange(collection.Cast<ListItem>().ToArray());
+            if (this._selectedItems == null)
+                return;
+            foreach (ListItem listItem in this._selectedItems)
+            {
+                //ListItem byValue = this.InnerListControl.Items.FindByValue(listItem.Value);
+                //if (byValue != null)
+                //    byValue.Selected = true;
+            }
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            //if (this.KeepHiddenValue && Settings.IsAnalyticsEnabled && this.FindControl(this.ID + "_complexvalue") == null)
+            //{
+            //    HiddenField hiddenField = new HiddenField();
+            //    hiddenField.ID = this.ID + "_complexvalue";
+            //    hiddenField.Value = (string)this.Result.Value;
+            //    this.Controls.AddAt(0, (Control)hiddenField);
+            //}
+            //this.title.AssociatedControlID = this.InnerListControl.ID;
+            base.OnPreRender(e);
+        }
+
+        [DefaultValue(5)]
+        [VisualFieldType(typeof(EditField))]
+        [VisualProperty("File size limit (MB) :", 5)]
+        [VisualCategory("Upload")]
+        public string FileSizeLimit
+        {
+            get
+            {
+                return this._fileSizeLimit.ToString();
+            }
+            set
+            {
+                int result;
+                if (!int.TryParse(value, out result))
+                    result = 5;
+                this._fileSizeLimit = result;
+            }
         }
     }
 }
